8bffc3f895e9c2f78e8f6b39527b374118c54a06
Nancy <nancy.richardson@onthebeach.co.uk>
2013-09-26 11:01:27 +0100

    Ready to test towards alive or dead logic against number of neighbours

--- game-of-life-spec.rb
+++ game-of-life-spec.rb
@@ -1,49 +1,36 @@
 describe 'playing game of life' do
+
 	subject {Game.new} 
+	
 	it 'creates empty array to represent grid' do
-		pending
 		expect(subject.grid).to be_a Array
 	end
 
 	it 'ticks over on empty grid' do
-		pending
 		expect(subject.tickover).to eq([])
 	end
 
 	it 'ticks over with one cell' do
-		pending
-		subject.grid << 1
-		expect( subject.tickover ).to eq([])
+		subject.grid = [[0,0]]
+		expect(subject.tickover).to eq([])
 	end
-	it 'extracts actual coordinate values' do
-		pending
-		subject.grid << [[0,0],[0,1],[0,2]]
-		subject.print_out
-		expect(subject.check_around).to eq([0,0])
-	end
+
 	it 'finds [0,1] exists and so [0,0] has a neighbour' do
-		pending
-		subject.grid << [[0,0],[0,1],[0,2]]
+		subject.grid = [[0,0],[0,1],[0,2]]
 		subject.check_around
-		expect(subject.check_around).to eq('you have 2 neighbour(s)')
+		expect(subject.check_around).to eq('you have 1 neighbour(s)')
 	end
+
 	it 'finds [0,0] has only 2 neighbour' do
 		subject.grid = [[0,0],[0,1],[-1,-1]]
 		subject.check_around
 		expect(subject.check_around).to eq('you have 2 neighbour(s)')
 	end
-	it 'returns a grid of 1 cells if seeded with 3 cells' do
-		pending
-		subject.grid << [[0,0],[0,1],[0,2]]
-		expect(subject.tickover).to eq([[0,1]])
-	end
 
 	it 'check for life' do
-		pending
+		subject.grid = [[0,0],[0,1],[0,2]]
 		expect(subject.grid.size).to_not eql(0)
 	end
-
-
 
 end
 
@@ -60,6 +47,8 @@ class Game
 			@grid = []
 		end
 		@grid
+
+		#@alive = @alive ? [2,3].include?(neighbours) : neighbours == 3
 	end
 
 	def check_around
@@ -80,16 +69,10 @@ class Game
 	end
 
 	def print_out
-		#puts "#{@grid[0].each}"
-
-		#			x=-1         x=n        x=+1
+		#					x=-1         x=n        x=+1
 		#y=n-1 	x=n-1,y=n-1   x=n,y=n-1  x=n+1,y=n-1	
 		#y=n   	x=n-1,y=n     x=0,y=0    x=n+1,y=n
 		#y=n+1 	x=n-1,y=n+1   x=n,y=n+1  x=n+1,y=n+1
-
 	end
-
-
-
 
 end
