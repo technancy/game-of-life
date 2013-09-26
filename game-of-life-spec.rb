9dd4e8db4fd9866a81aa78aa5118d0b4c34292b3
Nancy <nancy.richardson@onthebeach.co.uk>
2013-09-26 10:47:12 +0100

    Compare grid with possible neighbour matches - array intersection used

--- game-of-life-spec.rb
+++ game-of-life-spec.rb
@@ -1,14 +1,17 @@
 describe 'playing game of life' do
 	subject {Game.new} 
 	it 'creates empty array to represent grid' do
+		pending
 		expect(subject.grid).to be_a Array
 	end
 
 	it 'ticks over on empty grid' do
+		pending
 		expect(subject.tickover).to eq([])
 	end
 
 	it 'ticks over with one cell' do
+		pending
 		subject.grid << 1
 		expect( subject.tickover ).to eq([])
 	end
@@ -19,9 +22,15 @@ describe 'playing game of life' do
 		expect(subject.check_around).to eq([0,0])
 	end
 	it 'finds [0,1] exists and so [0,0] has a neighbour' do
+		pending
 		subject.grid << [[0,0],[0,1],[0,2]]
 		subject.check_around
-		expect(subject.check_around).to eq('you have a neighbour')
+		expect(subject.check_around).to eq('you have 2 neighbour(s)')
+	end
+	it 'finds [0,0] has only 2 neighbour' do
+		subject.grid = [[0,0],[0,1],[-1,-1]]
+		subject.check_around
+		expect(subject.check_around).to eq('you have 2 neighbour(s)')
 	end
 	it 'returns a grid of 1 cells if seeded with 3 cells' do
 		pending
@@ -43,7 +52,7 @@ class Game
 
 	def initialize
 		@grid = []
-		
+		@neighbour = 0		
 	end
 
 	def tickover
@@ -56,21 +65,27 @@ class Game
 	def check_around
 		x = 0
 		y = 0
-		@grid[0].each do |thing|
-			if thing == [x,y+2]
-				puts "you have a neighbour"
-				return "you have a neighbour"
-			end
-		end
+
+		matches = [[x-1,y-1], [x,y-1] , [x+1,y-1] , [x-1,y] , [x+1,y] , [x-1,y+1] , [x,y+1] , [x+1,y+1]]	
+		puts "MATCHES #{matches}"
+		puts "GRID = #{@grid.inspect}"	
+
+		intersection = @grid & matches
+			
+				if !intersection.empty?		
+					neighbour = intersection.size
+					return "you have #{neighbour} neighbour(s)"				
+				end
+
 	end
 
 	def print_out
 		#puts "#{@grid[0].each}"
 
-#			x=1         x=2        x=3
-#y=n-1 	x=n-1,y=n-1   x=n,y=n-1  x=n+1,y=n-1	
-#y=n   	x=n-1,y=n     x=2,y=3    x=n+1,y=n
-#y=n+1 	x=n-1,y=n+1   x=n,y=n+1  x=n+1,y=n+1
+		#			x=-1         x=n        x=+1
+		#y=n-1 	x=n-1,y=n-1   x=n,y=n-1  x=n+1,y=n-1	
+		#y=n   	x=n-1,y=n     x=0,y=0    x=n+1,y=n
+		#y=n+1 	x=n-1,y=n+1   x=n,y=n+1  x=n+1,y=n+1
 
 	end
 
