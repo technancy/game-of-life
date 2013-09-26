c0ed8db9a9864688bc1c1f9c8b9eedeb6fb0a944
Nancy <nancy.richardson@onthebeach.co.uk>
2013-09-26 07:42:30 +0100

    ready to implement check around logic

--- game-of-life-spec.rb
+++ game-of-life-spec.rb
@@ -12,15 +12,16 @@ describe 'playing game of life' do
 		subject.grid << 1
 		expect( subject.tickover ).to eq([])
 	end
-	it 'checks around extracts actual coordinate values' do
+	it 'extracts actual coordinate values' do
 		pending
 		subject.grid << [[0,0],[0,1],[0,2]]
 		subject.print_out
 		expect(subject.check_around).to eq([0,0])
 	end
-	it 'check_around finds [0,-1] does not exist in grid' do
+	it 'finds [0,1] exists and so [0,0] has a neighbour' do
 		subject.grid << [[0,0],[0,1],[0,2]]
-		expect(subject.check_around).to eq(nil)
+		subject.check_around
+		expect(subject.check_around).to eq('you have a neighbour')
 	end
 	it 'returns a grid of 1 cells if seeded with 3 cells' do
 		pending
@@ -53,16 +54,24 @@ class Game
 	end
 
 	def check_around
-		#@grid[0][0]
-		#@grid[0].each do |[]|
-		#	if [0,-1]
-				return nil
-		#	end
-		#end
+		x = 0
+		y = 0
+		@grid[0].each do |thing|
+			if thing == [x,y+2]
+				puts "you have a neighbour"
+				return "you have a neighbour"
+			end
+		end
 	end
 
 	def print_out
 		#puts "#{@grid[0].each}"
+
+#			x=1         x=2        x=3
+#y=n-1 	x=n-1,y=n-1   x=n,y=n-1  x=n+1,y=n-1	
+#y=n   	x=n-1,y=n     x=2,y=3    x=n+1,y=n
+#y=n+1 	x=n-1,y=n+1   x=n,y=n+1  x=n+1,y=n+1
+
 	end
 
 
